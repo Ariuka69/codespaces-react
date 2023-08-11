08d9bd8dbe636b60d02bf9bb483ce9729bb42c88
Ariuka69 <122322394+Ariuka69@users.noreply.github.com>
2023-08-11 07:12:04 +0000

    bla

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import Category from "./components/Category";
+import { ShoppingCartOutlined  } from '@ant-design/icons';
 
 const App = () => {
   return ( 
     <>
+    <div className="header">
+      <li><a href="#">Logo</a></li>
+      <button className="Sags"><ShoppingCartOutlined />Сагс</button>
+    </div>
     <Category/>
       </>
    );
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,20 +1,23 @@
 import React , {useEffect, useState} from  "react";
 import './Category.css'
+import { RightOutlined  } from '@ant-design/icons';
 const Category = () =>{
-  const [section, setSection] = useState("");
-  const api = () =>{
-    fetch('https://fakestoreapi.com/products/categories')
-        .then(res=>res.json())
-        .then(json=>console.log(json)
-        )
+  const [section, setSection] = useState([]);
+  const Api = async () => {
+    const i = await fetch("https://fakestoreapi.com/products/categories");
+    const a = await i.json();
+    console.log(a);
+    setSection(a);
   };
   useEffect(() =>{
-    api()
+    Api()
   },[])
   return (
-      <>
-        
-      </>
+      <div>
+        <div className="adasd">
+          {section.map(o => <li><a href="#" ><RightOutlined />{o}</a></li>)}
+        </div>
+      </div>
     ); 
 }
  
